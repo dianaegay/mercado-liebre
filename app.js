a62c5f1bc1db40f926e504c65ef94489219179df
dianaegay <dianaegay29@gmail.com>
2021-10-04 16:49:17 -0300

    archivos del proyecto

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => console.log('Servidor funcionando en host 3000'));
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req,res) => {
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.post('/register', (req,res) => {
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+app.post('/login', (req,res) => {
+    res.sendFile(__dirname + '/views/home.html');
+})
+
