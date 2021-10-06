0d076cc0434caea8583e1f4d077cf9004a5c8a92
dianaegay <dianaegay29@gmail.com>
2021-10-05 21:17:51 -0300

    configurando heroku y añadiendo procfile

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.listen (3000 || process.env.PORT, function () {
-    console.log('Servidor funcionando en host 3000');
-});
+
 
 app.use(express.static('public'));
 
@@ -23,5 +21,8 @@ app.get('/login', (req,res) => {
 });
 app.post('/login', (req,res) => {
     res.sendFile(__dirname + '/views/home.html');
-})
+});
 
+app.listen (3000 || process.env.PORT, function () {
+    console.log('Servidor funcionando en host 3000');
+});
